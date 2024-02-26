14f44a43a8f11f0a8f7bcf5bc3b78036858b8701
yadavn01 <ynaman29@gmail.com>
2024-02-26 21:41:15 +1300

    test commit

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Sweltering", "Scorching"
 };
 
 app.MapGet("/weatherforecast", () =>
