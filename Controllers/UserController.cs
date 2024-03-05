fc6b5efaea20a4bfb2e0ec6a8da413fad2937f35
yadavn01 <ynaman29@gmail.com>
2024-03-05 23:06:17 +1300

    set up the dev and prod environment

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -15,13 +15,14 @@ public class UserController : ControllerBase
 
     }
 
-    [HttpGet("test")]
-    public string[] Test()
+    [HttpGet("GetUsers/{testValue}")]
+    public string[] GetUsers(string testValue)
     {
         string[] responseArray = new string[] {
             "test1",
             "test2",
-            "test3"
+            "test3",
+            testValue
         };
         return responseArray;
     }
--- Program.cs
+++ Program.cs
@@ -6,17 +6,40 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+builder.Services.AddCors((options) =>
+{
+    options.AddPolicy("DevCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("http://localhost:4200", "http://localhost:3000", "http://localhost:8000")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+
+    });
+    options.AddPolicy("ProdCors", (corsBuilder) =>
+    {
+        corsBuilder.WithOrigins("https://myProductionSite.com")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors("DevCors");
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 else {
-app.UseHttpsRedirection();
+        app.UseCors("ProdCors");
+        app.UseHttpsRedirection();
 }
+
 app.MapControllers();
 
 
