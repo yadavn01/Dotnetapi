f1d78586e37807f044d6e703e2146efe6d51751e
Naman Yadav <naman.yadav@juvare.com>
2024-03-11 10:55:27 +1300

    controlles setup

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf7d7fa69ed518b649908ec63655b277762f1a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a8905c22596efb8192ccaf3a2883cf40c6cf474")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
