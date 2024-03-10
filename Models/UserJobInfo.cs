5e14f22a2e3e55d1f4bc18508e320d0e2dc2aa05
Naman Yadav <naman.yadav@juvare.com>
2024-03-11 10:29:39 +1300

    user userJobInfo and UserSalary models setup

---
+++ Models/UserJobInfo.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+namespace DotnetAPI
+{
+    public partial class UserJobInfo
+    {
+        public int UserId {get;set;}
+        public string JobTitle {get;set;}
+        public string Department {get;set;}
+
+        public UserJobInfo()
+        {
+            if (JobTitle == null)
+            {
+                JobTitle = "";
+            }
+            if (Department == null)
+            {
+                Department = "";
+            }
+        }
+    }
+}
