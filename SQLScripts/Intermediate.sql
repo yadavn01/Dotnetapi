ccf5a8ac616be7745f981fb397c581f6650c574c
yadavn01 <ynaman29@gmail.com>
2024-04-25 21:43:54 +1200

    table joins and cluster indexes

--- SQLScripts/Intermediate.sql
+++ SQLScripts/Intermediate.sql
@@ -12,3 +12,28 @@ FROM TutorialAppSchema.Users AS Users
     
 WHERE Users.Active = 1
 ORDER BY Users.UserId DESC
+
+
+SELECT  Users.UserId
+        , Users.FirstName + ' ' + Users.LastName AS FullName
+        , UserJobInfo.JobTitle
+        , UserJobInfo.Department
+        , UserSalary.Salary
+        , Users.Email
+        , Users.Gender
+        , Users.Active
+  FROM  TutorialAppSchema.Users AS Users
+      --INNER JOIN
+      JOIN TutorialAppSchema.UserSalary AS UserSalary
+          ON UserSalary.UserId = Users.UserId
+      LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+          ON UserJobInfo.UserId = Users.UserId
+ WHERE  Users.Active = 1
+ ORDER BY Users.UserId DESC;
+
+ CREATE CLUSTERED INDEX cix_UserSalary_UserId
+    ON TutorialAppSchema.UserSalary (UserId);
+
+CREATE NONCLUSTERED INDEX ix_UserJobInfo_JobTitle
+    ON TutorialAppSchema.UserJobInfo (JobTitle)
+    INCLUDE (Department);
